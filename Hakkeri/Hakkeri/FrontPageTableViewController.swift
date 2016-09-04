9e04c4b71fb012cf51cc0ac1b3466a9fa5af85cb
Sampsa Kuronen <sampsa.kuronen@reaktor.fi>
2016-09-04 23:33:11 +0300

    Working top stories

--- Hakkeri/Hakkeri/FrontPageTableViewController.swift
+++ Hakkeri/Hakkeri/FrontPageTableViewController.swift
@@ -1,36 +1,76 @@
-//
-//  FrontPageTableViewController.swift
-//  Hakkeri
-//
-//  Created by Sampsa Kuronen on 04.09.2016.
-//  Copyright © 2016 Sampsa Kuronen. All rights reserved.
-//
-
 import UIKit
+import SafariServices
+import Alamofire
+
 
 class FrontPageTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var topStories = [Int]()
+    
+    func setStatusBarStyle() {
         let height = UIApplication.shared.statusBarFrame.size.height
         let insets = UIEdgeInsets(top: height, left: 0, bottom: 0, right: 0)
         self.tableView.contentInset = insets
         self.tableView.scrollIndicatorInsets = insets
+        
+        let px = 1 / UIScreen.main.scale
+        let frame = CGRect(x: 0, y: 0, width: self.tableView.frame.size.width, height: px)
+        let line: UIView = UIView(frame: frame)
+        self.tableView.tableHeaderView = line
+        line.backgroundColor = self.tableView.separatorColor
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStatusBarStyle()
+        getTopStories()
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()    }
+        super.didReceiveMemoryWarning()
+    }
+    
+    func getTopStories() {
+        Alamofire.request("https://hacker-news.firebaseio.com/v0/topstories.json", withMethod: .get)
+            .responseJSON { response in
+                if let topIds = response.result.value as? [Int] {
+                    self.topStories = topIds
+                    self.tableView.reloadData()
+                }
+        }
+    }
 
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return topStories.count >= 100 ? 100 : topStories.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "StoryCell", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StoryCell", for: indexPath) as! StoryTableViewCell
+        Alamofire.request("https://hacker-news.firebaseio.com/v0/item/\(topStories[indexPath.row]).json", withMethod: .get)
+            .responseJSON { response in
+                if let story = response.result.value as? NSDictionary {
+                    
+                    let url = URL(string: story.object(forKey: "url")! as! String)
+                    let domain = url?.host
+                    
+                    cell.titleLabel.text = story.object(forKey: "title")! as? String
+                    cell.domainLabel.text = domain
+                    cell.url = url
+                    
+                    self.tableView.layoutSubviews()
+                }
+        }
+        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! StoryTableViewCell
+        let svc = SFSafariViewController(url: cell.url!, entersReaderIfAvailable: true)
+        self.present(svc, animated: true, completion: nil)
     }
     
     // MARK: - Navigation
---
+++ Hakkeri/Hakkeri/StoryTableViewCell.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+
+class StoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var domainLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var url: URL? = nil
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override func prepareForReuse() {
+        domainLabel.text = ""
+        titleLabel.text = ""
+    }
+
+}
