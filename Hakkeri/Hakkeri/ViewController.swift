ce2f543a800bf4981919bd9532f065c1b088f554
Sampsa Kuronen <sampsa.kuronen@reaktor.com>
2017-07-16 12:04:37 +0300

    Start rewrite with a fresh project

---
+++ Hakkeri/Hakkeri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hakkeri
+//
+//  Created by Sampsa Kuronen on 16/07/2017.
+//  Copyright © 2017 Sampsa Kuronen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
